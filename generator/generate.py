c79107e895f466e1cd834eebbdff076766ac1430
casswirxx <meshkova.ekamax@gmail.com>
2026-06-13 15:31:47 +0300

    Добавлена категория

--- generator/generate.py
+++ generator/generate.py
@@ -3,16 +3,16 @@ import random
 import os
 import sys
 
-NUM_ROWS = 50
-COLUMNS = ["COLUMN_1", "COLUMN_2", "COLUMN_3", "COLUMN_4"]
+NUM_ROWS = 67
+COLUMNS = ["Kind", "Number_of_eyes", "Height", "Works_for_Gru"]
 
 def generate_row():
 
     return {
-        "COLUMN_1": random.randint(0, 100),
-        "COLUMN_2": round(random.uniform(1.5, 9.9), 2),
-        "COLUMN_3": random.randint(0, 100),
-        "COLUMN_4": random.choice(["A", "B", "C"]),
+        "Kind": random.choice(["ordinary", "mutated"]),
+        "Number_of_eyes": random.randint(1, 2),
+        "Height": round(random.uniform(94, 120), 2),
+        "Works_for_Gru": random.choice(["yes", "no"]),
     }
 
 OUTPUT_DIR = sys.argv[1] if len(sys.argv) > 1 else "/data"
