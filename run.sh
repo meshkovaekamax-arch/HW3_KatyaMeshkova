f07e3fd8ed202154bf0182f977b990ba6b58bb7c
casswirxx <meshkova.ekamax@gmail.com>
2026-06-13 17:56:17 +0000

    minor changes

--- run.sh
+++ run.sh
@@ -77,7 +77,6 @@ case "$1" in
             exit 1
         fi
         docker rm -f new-web-report 2>/dev/null
-        chmod -R a+r data/
         cp data/report.html data/index.html
         docker run -d -p 8080:80 -v "$(pwd)/data:/usr/share/nginx/html:ro" --name new-web-report nginx
     ;;
